c8b8709ccdfbbd4d2a071225fd42af272166f5ec
Adrian Werimo <adrwer7@gmail.com>
2020-07-27 18:32:35 +0300

    Constant vs Read-only fields

--- Prestige.Biz/Program.cs
+++ Prestige.Biz/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prestige.Biz
 {
     class Program
     {
+        /* Constant*/                               /* Read-only */
+        // Compile-time constant                    Runtime constant
+        // Assigned on declaration                  Assigned on declaration or constructor
+        // Only a number, string or boolean         Any data type
+        // Always static                            Optionally static
+
+        public const int ConstantNumber = 100;
+        public static readonly int ReadOnlyNumber = 0;
+
+        static Program()
+        {
+            ReadOnlyNumber = 10;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(ConstantNumber);
+            Console.WriteLine(ReadOnlyNumber);
+            Console.ReadKey();
         }
     }
 }
